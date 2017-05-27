cee4e9f5a00b0d840d676aa322befe00112a00d3
Kendrick Shaw <kms15@case.edu>
2017-05-27 02:38:48 +0000

    complete earpiece mold

--- CADQuery/EarpieceMold.py
+++ CADQuery/EarpieceMold.py
@@ -30,9 +30,10 @@ def square_dish(workplane, brim_thickness, brim_width,
     return body.cut(cutout)
 
 
+inner_width = 20
+inner_length = 40
+
 def mold_dish(workplane, thickness):
-    inner_width = 30
-    inner_length = 60
     depth = 20
     padding = 5
     dish = square_dish(workplane,
@@ -54,13 +55,44 @@ def mold_dish(workplane, thickness):
 part_x_spacing = 75
 part_y_spacing = 100
 
-bottom = mold_dish(cq.Workplane("XY"), 10)
+big_radius = 15./2
+hole_depth = 3
+center_offset = 10
+earbud_centers = [(0, -10), (0, 10)]
+post_radius = 1./8 * mm_per_inch * 1./2
+
+bottom_dish = mold_dish(cq.Workplane("XY"), big_radius + hole_depth + 1)
+bottom_dish_floor = bottom_dish.faces("+Z exc >Z").workplane()
+bottom = bottom_dish_floor.pushPoints(earbud_centers).circle(post_radius) \
+    .cutBlind(-hole_depth + -big_radius) \
+    .cut(
+        bottom_dish.union(
+            bottom_dish_floor.pushPoints(earbud_centers)
+            .sphere(big_radius, combine=False),
+            combine=False
+        )
+    )
 show(bottom)
 
-middle = mold_dish(cq.Workplane("XY", origin=(part_x_spacing, 0, 0)), 10)
+
+def make_cone(pnt):
+            (xp, yp, zp) = pnt.toTuple()
+
+            return cq.Solid.makeCone(big_radius, 0, 30, cq.Vector(xp, yp, zp))
+
+middle_dish = mold_dish(cq.Workplane("XY", origin=(part_x_spacing, 0, 0)), 10)
+middle_dish_bottom = middle_dish.faces("<Z").workplane(invert=True)
+middle = middle_dish.cut(middle_dish.union(
+    middle_dish_bottom.pushPoints(earbud_centers).eachpoint(make_cone, True),
+    combine=False
+    ))
 show(middle)
 
-top = mold_dish(cq.Workplane("XY", origin=(0, part_y_spacing, 0)), 5)
+top_dish = mold_dish(cq.Workplane("XY", origin=(0, part_y_spacing, 0)), 5)
+top_dish_floor = top_dish.faces("+Z exc >Z").workplane()
+top = top_dish_floor.pushPoints(earbud_centers).hole(2*post_radius) \
+    .rect(inner_width, 2*(center_offset - big_radius)) \
+    .cutThruAll()
 show(top)
 
 # result = mold_dish(
